96ef72fa05c40eecc5c1c6ab6a2aa60f5aea5bda
Rishikesh Sharma <srishikesh39@gmai.com>
2026-06-08 10:18:50 +0530

    Remove duplicate terraform blocks in modules

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
-  }
-}
-
 variable "name" { type = string }
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
