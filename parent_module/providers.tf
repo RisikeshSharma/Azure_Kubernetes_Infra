1fb6311d49675d0f3f743df95cf9e126ebe4e0e9
Rishikesh Sharma <srishikesh39@gmai.com>
2026-06-09 11:12:56 +0530

    Added Terraform required version constraint

--- parent_module/providers.tf
+++ parent_module/providers.tf
@@ -1,9 +1,16 @@
 terraform {
+
+  required_version = ">= 1.8.0"
+
   required_providers {
+
     azurerm = {
+
       source  = "hashicorp/azurerm"
-      version = "4.76.0"
+      version = "~>4.0"
+
     }
+
   }
   backend "azurerm" {
     resource_group_name  = "Vaishno-RG"
@@ -11,6 +18,7 @@ terraform {
     container_name       = "vaishnocontainer"
     key                  = "terraform.tfstate"
   }
+
 }
 provider "azurerm" {
   features {
