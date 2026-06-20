651df9102643afaaeede928edc24aa6e4768337b
Rishikesh Sharma <srishikesh39@gmail.com>
2026-06-20 21:35:26 +0000

    Change kubernetes_version type from number to string

--- parent_module/variable.tf
+++ parent_module/variable.tf
@@ -13,7 +13,7 @@ variable "aks_cluster" {
     location            = string
     resource_group_name = string
     dns_prefix          = string
-    kubernetes_version = number
+    kubernetes_version =  string
 
     node_pool = object({
       name       = string
